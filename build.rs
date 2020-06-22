5ffeb78ab599d44d66f17e4ad0244454ef311150
charles-r-earp <charles.r.earp@gmail.com>
2020-06-22 16:51:15 -0700

    Update build.rs

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ fn main() {
       .define("DNNL_LIBRARY_TYPE", "STATIC")
       .define("DNNL_BUILD_EXAMPLES", "OFF")
       .define("DNNL_BUILD_TESTS", "OFF")
-      .cxx_flag("-fPIC")
+      .cxxflag("-fPIC")
       .build();
     println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
     println!("cargo:rustc-link-lib=static=dnnl");
