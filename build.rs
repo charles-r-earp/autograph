88a3a264692b84dd87a13617287acd3e28f93c15
charles-r-earp <charles.r.earp@gmail.com>
2020-06-25 15:56:42 -0700

    Added options to examples to enable testing of 1 epoch on ci. Updated workflow. Build script now only links to omp without supplying path, since this path is target specific.

--- build.rs
+++ build.rs
@@ -10,45 +10,11 @@ fn main() {
       .build();
     println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
     println!("cargo:rustc-link-lib=static=dnnl");
+    println!("cargo:rustc-link-lib=dylib=gomp");
     cpp_build::Config::new()
       .include(dst.join("include").display().to_string())
       .build("src/lib.rs");
-  }
-  /*{
-    // gomp for dnnl
-    if cfg!(target_family = "unix") {
-      let machine = Command::new("gcc")
-        .arg("-dumpmachine")
-        .output()
-        .unwrap()
-        .stdout;
-      let machine = CString::new(machine)
-        .unwrap()
-        .into_string()
-        .unwrap();
-      let machine = machine.lines()
-        .next()
-        .unwrap();
-      let version = Command::new("gcc")
-        .arg("-dumpversion")
-        .output()
-        .unwrap()
-        .stdout;
-      let version = CString::new(version)
-        .unwrap()
-        .into_string()
-        .unwrap();
-      let version = version.lines()
-        .next()
-        .unwrap();
-      println!("cargo:rustc-link-search=native=/usr/lib/gcc/{}/{}", machine, version);
-      println!("cargo:rustc-link-lib=dylib=gomp");
-    }
-    else if cfg!(target_family = "windows") {
-      
-    }
-  }*/
-    
+  }  
   #[cfg(feature="cuda")]
   {
     { // compile custom cuda source
